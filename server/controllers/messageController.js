27da78c4a298a87ddb88185fc0bd72bd340d51a6
kp5406mbi-cloud <kp5406.mbi@gmail.com>
2026-05-29 01:07:40 +0530

    Fixed chat sender population and alignment

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,70 +1,116 @@
-const Message = require("../models/Message");
+const Message =
+  require("../models/Message");
 
-const sendMessage = async (req, res) => {
+const sendMessage =
+  async (req, res) => {
 
-  try {
+    try {
 
-    const message = await Message.create({
+      const message =
+        await Message.create({
 
-      sender: req.user.id,
+          sender:
+            req.user.id,
 
-      receiver: req.body.receiver,
+          receiver:
+            req.body.receiver,
 
-      text: req.body.text
+          text:
+            req.body.text
 
-    });
+        });
 
-    res.status(201).json(message);
+      const populatedMessage =
+        await Message.findById(
+          message._id
+        ).populate(
+          "sender",
+          "_id name"
+        );
 
-  }
+      res.status(201).json(
+        populatedMessage
+      );
 
-  catch (error) {
+    }
 
-    res.status(500).json({
-      message: error.message
-    });
+    catch (error) {
 
-  }
+      res.status(500).json({
+
+        message:
+          error.message
+
+      });
+
+    }
 
 };
 
-const getMessages = async (req, res) => {
+const getMessages =
+  async (req, res) => {
 
-  try {
+    try {
 
-    const messages = await Message.find({
+      const messages =
+        await Message.find({
 
-      $or: [
+          $or: [
 
-        {
-          sender: req.user.id,
-          receiver: req.params.userId
-        },
+            {
 
-        {
-          sender: req.params.userId,
-          receiver: req.user.id
-        }
+              sender:
+                req.user.id,
 
-      ]
+              receiver:
+                req.params.userId
 
-    }).sort({ createdAt: 1 });
+            },
 
-    res.json(messages);
+            {
 
-  }
+              sender:
+                req.params.userId,
 
-  catch (error) {
+              receiver:
+                req.user.id
 
-    res.status(500).json({
-      message: error.message
-    });
+            }
 
-  }
+          ]
+
+        })
+
+        .populate(
+          "sender",
+          "_id name"
+        )
+
+        .sort({
+          createdAt: 1
+        });
+
+      res.json(messages);
+
+    }
+
+    catch (error) {
+
+      res.status(500).json({
+
+        message:
+          error.message
+
+      });
+
+    }
 
 };
 
 module.exports = {
+
   sendMessage,
+
   getMessages
+
 };
